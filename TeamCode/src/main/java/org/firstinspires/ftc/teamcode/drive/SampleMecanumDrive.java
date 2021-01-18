71ddd59022d21814b251e8bc075fe4cc26660aee
BuildTools <unconfigured@null.spigotmc.org>
2021-01-18 12:42:32 -0600

    IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -131,7 +131,7 @@ public class SampleMecanumDrive extends MecanumDrive {
 
         // TODO: if your hub is mounted vertically, remap the IMU axes so that the z-axis points
         // upward (normal to the floor) using a command like the following:
-        BNO055IMUUtil.remapAxes(imu, AxesOrder.XYZ, AxesSigns.PNP);
+        //BNO055IMUUtil.remapAxes(imu, AxesOrder.XYZ, AxesSigns.PNP);
 
         leftFront = (ExpansionHubMotor) hardwareMap.get("leftFront");
         leftRear = (ExpansionHubMotor) hardwareMap.get("leftRear");
